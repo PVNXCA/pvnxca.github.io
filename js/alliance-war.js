ad227e7e7d38f2c65b80e53dd1acd83550362903
PVNXCA <pvnxca@gmail.com>
2026-04-29 12:19:18 +0200

    Update alliance-war.js (simplification of use)

--- js/alliance-war.js
+++ js/alliance-war.js
@@ -7,7 +7,6 @@ function setOrientationClass() {
 		if (window.innerWidth > window.innerHeight) {
 			CurrentHtml.classList.add('html--horizontal');
 			CurrentHtml.classList.remove('html--vertical');
-			console.log(window.innerHeight + ' / ' + window.innerWidth + ' = ' + window.innerHeight / window.innerWidth);
 			if (window.innerHeight / window.innerWidth <= 0.5) {
 				CurrentHtml.classList.add('html--ratio-by-height');
 				CurrentHtml.classList.remove('html--ratio-by-width');
@@ -22,7 +21,6 @@ function setOrientationClass() {
 		} else {
 			CurrentHtml.classList.add('html--vertical');
 			CurrentHtml.classList.remove('html--horizontal');
-			console.log(window.innerWidth + ' / ' + window.innerHeight + ' = ' + window.innerWidth / window.innerHeight);
 			if (window.innerWidth / window.innerHeight <= 0.5) {
 				CurrentHtml.classList.add('html--ratio-by-height');
 				CurrentHtml.classList.remove('html--ratio-by-width');
@@ -45,19 +43,32 @@ window.onresize = function() {
 
 // Get the colors order by url parameters
 
-let ColorObject = {
-	turquoise: {name:'Turquoise', backgroundColor:'#129b90', borderColor:'#d6dee3'}, // 00897f
-	lightblue: {name:'Bleu clair', backgroundColor:'#447ab1', borderColor:'#d6dee3'}, // 386ea5
-	darkblue: {name:'Bleu foncé', backgroundColor:'#2c29b1', borderColor:'#d6dee3'}, // 312ec4
-	pink: {name:'Rose', backgroundColor:'#c63fa7', borderColor:'#d6dee3'}, // b3008c
-	red: {name:'Rouge', backgroundColor:'#8e0000', borderColor:'#d6dee3'},
-	orange: {name:'Orange', backgroundColor:'#8f5514', borderColor:'#d6dee3'},
-	yellow: {name:'Jaune', backgroundColor:'#917b12', borderColor:'#d6dee3'},
-	brown: {name:'Marron', backgroundColor:'#522605', borderColor:'#d6dee3'},
-	purple: {name:'Violet', backgroundColor:'#612461', borderColor:'#d6dee3'},
-	green: {name:'Vert', backgroundColor:'#006f48', borderColor:'#d6dee3'}, // 00935e
-	frenchy: {name:'Frenchy', backgroundColor:'#1e981c', borderColor:'#04de00'}
-};
+let ColorArray = [
+	{name:'Frenchy', backgroundColor:'#1e981c', borderColor:'#04de00'},
+	{name:'Turquoise', backgroundColor:'#129b90', borderColor:'#d6dee3'}, // 00897f
+	{name:'Bleu clair', backgroundColor:'#447ab1', borderColor:'#d6dee3'}, // 386ea5
+	{name:'Bleu foncé', backgroundColor:'#2c29b1', borderColor:'#d6dee3'}, // 312ec4
+	{name:'Rose', backgroundColor:'#c63fa7', borderColor:'#d6dee3'}, // b3008c
+	{name:'Rouge', backgroundColor:'#8e0000', borderColor:'#d6dee3'},
+	{name:'Orange', backgroundColor:'#8f5514', borderColor:'#d6dee3'},
+	{name:'Jaune', backgroundColor:'#917b12', borderColor:'#d6dee3'},
+	{name:'Marron', backgroundColor:'#522605', borderColor:'#d6dee3'},
+	{name:'Violet', backgroundColor:'#612461', borderColor:'#d6dee3'},
+	{name:'Vert', backgroundColor:'#006f48', borderColor:'#d6dee3'}, // 00935e
+];
+
+let CurrentParameters = new URLSearchParams(window.location.search);
+if (CurrentParameters == '') {
+	window.location.replace(window.location.href + '?frenchy=0');
+}
+
+let frenchyNumber;
+if (CurrentParameters.get('frenchy')) {
+	frenchyNumber = Number(CurrentParameters.get('frenchy'));
+	if (frenchyNumber > 0) {
+		ColorArray[frenchyNumber] = ColorArray[0];
+	}
+}
 
 let CurrentHead = document.querySelector('head');
 let GeneratedStyle = document.createElement('style');
@@ -69,10 +80,6 @@ GeneratedStyle.textContent += ''
 	+ ':root {'
 ;
 
-let CurrentParameters = new URLSearchParams(window.location.search);
-if (CurrentParameters == '') {
-	window.location.replace(window.location.href + '?color1=turquoise&color2=lightblue&color3=darkblue&color4=pink&color5=red&color6=orange&color7=yellow&color8=brown&color9=purple&color10=green');
-}
 let menuButton;
 for (let i = 1; i < 11; i++) {
 	menuButton = document.querySelector('.alliance-war__menu__button--' + i);
@@ -83,27 +90,31 @@ for (let i = 1; i < 11; i++) {
 			item.classList.toggle('alliance-war__grid__edge__sector__team--off');
 		});
 	});
-	if (CurrentParameters.get('color' + i)) {
-		GeneratedStyle.textContent += ''
-			+ '\n'
-				+ '	/* Color ' + i + ' */'
-			+ '\n'
-				+ '	--team-' + i + '-background-color: ' + ColorObject[CurrentParameters.get('color' + i)]['backgroundColor'] + ';'
-			+ '\n'
-				+ '	--team-' + i + '-border-color: ' + ColorObject[CurrentParameters.get('color' + i)]['borderColor'] + ';'
-		;
-		menuButton.innerHTML = ColorObject[CurrentParameters.get('color' + i)]['name'];
-		
+	
+	if (frenchyNumber === undefined) {
 		if (CurrentParameters.get('color' + i) == 'frenchy') {
-			LgWarContent.classList.add('alliance-war-' + i);
-			
-			document.querySelectorAll('.alliance-war__grid__edge__sector__team--' + i).forEach(function(item) {
-				item.parentNode.insertAdjacentHTML('beforeEnd', ''
-					+ '<div class="alliance-war__grid__edge__sector__frenchy"></div>'
-				);
-			});
+			frenchyNumber = i;
+			ColorArray[i] = ColorArray[0];
 		}
+	} else if (frenchyNumber == i) {
+		LgWarContent.classList.add('alliance-war-' + i);
+		
+		document.querySelectorAll('.alliance-war__grid__edge__sector__team--' + i).forEach(function(item) {
+			item.parentNode.insertAdjacentHTML('beforeEnd', ''
+				+ '<div class="alliance-war__grid__edge__sector__frenchy"></div>'
+			);
+		});
 	}
+	
+	GeneratedStyle.textContent += ''
+		+ '\n'
+			+ '	/* Color ' + i + ' */'
+		+ '\n'
+			+ '	--team-' + i + '-background-color: ' + ColorArray[i]['backgroundColor'] + ';'
+		+ '\n'
+			+ '	--team-' + i + '-border-color: ' + ColorArray[i]['borderColor'] + ';'
+	;
+	menuButton.innerHTML = ColorArray[i]['name'];
 }
 
 GeneratedStyle.textContent += ''
